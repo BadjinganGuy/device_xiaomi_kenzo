e278b04db69d4bba6a412be6e8d0c83fb933be18
#BadjinganGuy <badjingan@xiaome.me>
2019-02-09 03:34:54 +0700

    Havoc Lite

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,3 +1,3 @@
-add_lunch_combo superior_kenzo-user
-add_lunch_combo superior_kenzo-userdebug
-add_lunch_combo superior_kenzo-eng
+add_lunch_combo havoc_kenzo-user
+add_lunch_combo havoc_kenzo-userdebug
+add_lunch_combo havoc_kenzo-eng
