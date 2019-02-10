14064d05398296764ce7e3b6ebf7c67df86d6462
Ангга Роми Сапутра <help@h1t.me>
2019-02-10 04:58:17 +0300

    Syberia

--- syberia_kenzo.mk
+++ syberia_kenzo.mk
@@ -17,13 +17,16 @@
 $(call inherit-product, device/xiaomi/kenzo/full_kenzo.mk)
 
 # Inherit some common Superior stuff.
-$(call inherit-product, vendor/havoc/config/common.mk)
+$(call inherit-product, vendor/syberia/common.mk)
+
+#Boot Animation res
+TARGET_BOOT_ANIMATION_RES := 1080
 
 # Set those variables here to overwrite the inherited values.
 BOARD_VENDOR := Xiaomi
 PRODUCT_BRAND := Xiaomi
 PRODUCT_DEVICE := kenzo
-PRODUCT_NAME := havoc_kenzo
+PRODUCT_NAME := syberia_kenzo
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_MODEL := Redmi Note 3
 TARGET_VENDOR := Xiaomi
@@ -37,7 +40,7 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
 BUILD_FINGERPRINT=Xiaomi/kenzo/kenzo:6.0.1/MMB29M/V8.2.1.0.MHOCNDL:user/release-keys
 
 # Build Official HavocOS
-HAVOC_BUILD_TYPE := Lite
+# HAVOC_BUILD_TYPE := Lite
 
 # Define host and user
 KBUILD_BUILD_HOST=Telegram.org
--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,3 +1,3 @@
-add_lunch_combo havoc_kenzo-user
-add_lunch_combo havoc_kenzo-userdebug
-add_lunch_combo havoc_kenzo-eng
+add_lunch_combo syberia_kenzo-user
+add_lunch_combo syberia_kenzo-userdebug
+add_lunch_combo syberia_kenzo-eng
